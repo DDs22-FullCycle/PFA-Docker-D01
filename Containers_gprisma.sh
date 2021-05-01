7ed41dd4d500bdea77b1cc2af29a6664319175bb
DDs22 <douglasdossantos22@gmail.com>
2021-05-01 10:30:54 -0300

    conexão com o banco de dados adicionada - Prisma 2

---
+++ Containers_gprisma.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+docker exec -it node-container npx prisma generate
--- node/app/index.js
+++ node/app/index.js
@@ -2,10 +2,40 @@ const { response } = require('express')
 const express = require('express')
 const app = express()
 
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
+
+
+
 app.get('/', (req, res) => {
-    res.send('asdasssssssssssssssd')
+    
+    main().then((result) => {
+      return res.send( 
+        result 
+      )
+
+    }).catch((err) => {
+      
+    })
+
 })
 
 app.listen(3000, ()=> {
     console.log("Rodando na porta "+ app)
 })
+
+
+async function main() {
+    const allCadastros = await prisma.cadastro.findMany()
+     //prisma.user.findMany()
+     //console.log(allCadastros)
+    return allCadastros
+}
+
+main()
+  .catch(e => {
+    throw e
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
