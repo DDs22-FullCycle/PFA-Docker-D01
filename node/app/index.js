0c7b47c06d7fdaae8a9bcba0ea67ff4427143ea0
DDs22 <douglasdossantos22@gmail.com>
2021-05-02 22:46:19 -0300

    Finalização leitura BD Mysql

--- node/app/index.js
+++ node/app/index.js
@@ -7,11 +7,27 @@ const prisma = new PrismaClient()
 
 
 
+
 app.get('/', (req, res) => {
     
     main().then((result) => {
-      return res.send( 
-        result 
+
+      
+      var retorno = `<h2> Douglas dos Santos </h2>
+                     <h1> Lista de Cursos FullCycle: </h1>`
+          for (const iterator of result) {
+            retorno += "<h3><li>"+ iterator + "</li></h3>"   
+          }
+
+      return res.send(
+
+        //{
+          
+          retorno
+          
+
+        //}
+        
       )
 
     }).catch((err) => {
@@ -21,7 +37,7 @@ app.get('/', (req, res) => {
 })
 
 app.listen(3000, ()=> {
-    console.log("Rodando na porta "+ app)
+    console.log("Rodando naca porta "+ app)
 })
 
 
@@ -29,7 +45,16 @@ async function main() {
     const allCadastros = await prisma.cadastro.findMany()
      //prisma.user.findMany()
      //console.log(allCadastros)
-    return allCadastros
+
+     var lista = [];
+     for (const key in allCadastros) {
+       if (Object.hasOwnProperty.call(allCadastros, key)) {
+         const element = allCadastros[key].CadastroName;
+         lista.push(element)
+       }
+     }
+
+    return lista
 }
 
 main()
