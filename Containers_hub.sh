87aa4a309c3abcc8f5233b85c13beb5212d8602e
DDs22 <douglasdossantos22@gmail.com>
2021-05-07 07:36:22 -0300

    ajustes 2021-05-07 01

---
+++ Containers_hub.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+case "${1}" in
+
+    push)
+        echo "[=>   ] ..."
+        echo "[==>  ] ..."
+        echo "[===> ] ..."
+        echo "[#####] Concluído."    
+        # Comandos    
+    ;;
+    
+    pull)
+        echo "[=>   ] ..."
+        echo "[==>  ] ..."
+        echo "[===> ] ..."
+        echo "[#####] Concluído."  
+        # Comandos    
+    ;;
+
+    *)
+        echo "[=>   ] ..."
+        echo "[==>  ] ..."
+        echo "[===> ] ..."
+        echo "[#####] Concluído."  
+        # Comandos    
+    ;;
+esac
+
--- node/app/index.ts
+++ node/app/index.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from "@prisma/client";
-import express = require('express');
+import express from "express";
 
 const app = express();
 const prisma = new PrismaClient();
